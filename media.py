13d8d2fa4d93ea1ee996352745aafd527f49f709
Ryan <nygardryan@yahoo.com>
2018-02-22 20:38:00 -0700

    added comments to files

--- media.py
+++ media.py
@@ -1,12 +1,13 @@
 import webbrowser
-
+# Creates class for object instantiation
 class Movie():
   VALID_RATINGS = ["PG-13", "R", "X", "XX", "XXX"]
+# Initiation method
   def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
     self.title = movie_title
     self.storyline = movie_storyline
     self.poster_image_url = poster_image
     self.trailer_youtube_url = trailer_youtube
+# Method for showing youtube trailer
   def show_trailer(self):
     webbrowser.open(self.trailer_youtube_url)
-
--- readme.md
+++ readme.md
@@ -1,7 +1,13 @@
 Instructions on launching Movie Trailer Website
 
 1. Download from the online repository online at
+  https://github.com/dragyn5/movie_website_python_udacity
 
 2. Open up the Terminal on mac through the launchpad
 
-2. 
+3. Navigate to folder with website files
+
+4. Run Movie Trailer Website with 
+  ```python entertainment_center.py```
+
+5. Have fun with it!
